a980f87e1cd61db9af4bee71b9d7fa907f353569
DESKTOP-9AL2P16\user2 <kms6210123@gmail.com>
2023-07-19 20:37:35 +0900

    관리자 로그인 성공

---
+++ ALL_IS_WELL(Java)/ALL_IS_WELL/src/main/java/com/kh/app/admin/vo/AdminVo.java
@@ -0,0 +1,11 @@
+package com.kh.app.admin.vo;
+
+import lombok.Data;
+
+@Data
+public class AdminVo {
+	private String no;
+	private String adminId;
+	private String adminPwd;
+	private String adminNick;
+}
--- ALL_IS_WELL(Java)/ALL_IS_WELL/src/main/java/com/kh/app/login/controller/LoginController.java
+++ ALL_IS_WELL(Java)/ALL_IS_WELL/src/main/java/com/kh/app/login/controller/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.app.admin.service.AdminService;
+import com.kh.app.admin.vo.AdminVo;
 import com.kh.app.member.service.MemberService;
 import com.kh.app.member.vo.MemberVo;
 
@@ -18,7 +20,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LoginController {
 
-	private final MemberService service;
+	private final MemberService ms;
+	private final AdminService as;
 	
 	@GetMapping("login")
 	public String login() {
@@ -27,16 +30,27 @@ public class LoginController {
 	
 	@PostMapping("login")
 	public String login(MemberVo vo, HttpSession session) {
-		System.out.println("1");
-		MemberVo loginMember = service.login(vo);
+		MemberVo loginMember = ms.login(vo);
 		session.setAttribute("loginMember", loginMember);
-		
+		if(loginMember == null) {
+			return "error/404page";
+		}
 		return "main/Mmain";
 	}
 	
 	@GetMapping("Alogin")
 	public String Alogin() {
 		return "login/Alogin";
+	}
+	
+	@PostMapping("Alogin")
+	public String Alogin(AdminVo vo, HttpSession session) {
+		System.out.println(vo);
+		System.out.println(vo.getAdminId());
+		if(vo.getAdminId().equals("ADMIN") && vo.getAdminPwd().equals("1234")) {
+			return "main/Amain";
+		}
+		return "error/404page";
 	}
 	
 	@GetMapping("idFind")
