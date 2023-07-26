af04a687a4e74fefdda757eec1f598d3efde0cf1
DESKTOP-2UJDIJD\user2 <chul123369@gmail.com>
2023-07-26 21:32:30 +0900

    출장 문서 결재 트랜잭션 처리 ( 상세조회 작성 중 )

---
+++ ALL_IS_WELL(Java)/ALL_IS_WELL/src/main/java/com/kh/app/approval/vo/VacationApprovalVo.java
@@ -0,0 +1,16 @@
+package com.kh.app.approval.vo;
+
+import lombok.Data;
+
+@Data
+public class VacationApprovalVo {
+
+	private String no;
+	private String approvalDocumentNo;
+	private String no2;
+	private String content;
+	private String useDate;
+	private String startDate;
+	private String endDate;
+	
+}
